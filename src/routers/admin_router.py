ef69bd4dc972fd2cdfc998f4e12b6f5dfd4c4008
nariman079 <nariman079i@mail.ru>
2025-01-28 16:53:34 +0300

    added endpoint for create product

--- src/routers/admin_router.py
+++ src/routers/admin_router.py
@@ -1,24 +1,92 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, Body, UploadFile, HTTPException
 
 from src.depends import get_user
+from src.models import Product, Category, Image, Unit
+from src.schemas.product_schemas import (
+    ProductCreateSchema,
+    ProductDetailDisplaySchema, 
+    ProductUpdateSchema, 
+    UnitCreateSchema, 
+    CategoryCreateSchema,
+    CategoryDisplaySchema
+    )
 
 admin_router = APIRouter(tags=["admin"])
 
-
-@admin_router.get("/products/")
-async def get_full_cart(
-    user: Annotated[get_user, Depends()]
+@admin_router.post(
+    '/v1/admin/categories/', 
+    response_model=CategoryDisplaySchema
+)
+async def create_cateogry(
+    new_category: Annotated[
+        CategoryCreateSchema, 
+        Body()
+    ] 
 ):
-    """Получение товаров"""
-    pass
+    """Создание категории"""
+    try:
+        category = await Category.create(
+            **new_category.model_dump()
+            )
+    except ValueError as ex:
+        raise HTTPException(detail=str(ex))
+    return category
+
+@admin_router.post('/v1/admin/units/')
+async def create_unit(
+    new_unit: Annotated[UnitCreateSchema, Body()]
+):
+    """Создание товара"""
+    try:
+        unit = await Unit.create(**new_unit.model_dump())
+    except ValueError as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
+    return unit
 
 @admin_router.post(
-    "/products/", 
-    status_code=status.HTTP_201_CREATED
+    '/v1/admin/products/', 
 )
-async def create_product(user: Annotated[get_user, Depends()]):
+async def create_product(
+    new_product: Annotated[
+        ProductCreateSchema, 
+        Body()
+    ]
+    ):
     """Создание товара"""
+    try:
+        product = await Product.create(**new_product.model_dump())
+    except ValueError as ex:
+        raise HTTPException(status_code=400, detail=str(ex))
+    return product
+    
+
+@admin_router.get('/v1/admin/products/{product_id}')
+async def delete_product():
+    """Удаление товара"""
     pass
+
+@admin_router.get('/v1/admin/products/{product_id}')
+async def product():
+    """Получение товара"""
+    pass
+
+@admin_router.get('/v1/admin/products/')
+async def product():
+    """Получение списка товаров"""
+    pass
+
+@admin_router.get('/v1/admin/products/')
+async def product():
+    """Получение списка товаров с фильтрацией"""
+    pass
+
+@admin_router.get('/v1/admin/products/')
+async def product():
+    """Изменение товара"""
+    pass
+
+
+
 
